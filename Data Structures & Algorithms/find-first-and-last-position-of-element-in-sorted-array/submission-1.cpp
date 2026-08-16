d0d8b338cbfc59c9b4f0c7493b8ad20f48b90a75
Satvik-177 <102582656+Satvik-177@users.noreply.github.com>
2026-08-16 10:55:32 +0530

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-1.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int leftBS(vector<int>&nums, int target, int n){
+
+        int s=0;
+        int e=n-1;
+        int ans = -1;
+
+        while(s<=e){
+
+            int mid = s+((e-s)/2);
+
+            if(nums[mid] == target){
+                ans = mid;
+                e=mid-1;
+            }
+            else if(nums[mid] < target){
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+
+        return ans;
+    }
+
+    int rightBS(vector<int>&nums, int target, int n){
+
+        int s=0;
+        int e=n-1;
+        int ans = -1;
+
+        while(s<=e){
+
+            int mid = s+((e-s)/2);
+
+            if(nums[mid] == target){
+                ans = mid;
+                s=mid+1;
+            }
+            else if(nums[mid] < target){
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        vector<int>res(2,-1);
+
+        int n = nums.size();
+        int FirstOcc = leftBS(nums,target,n);
+        int LastOcc = rightBS(nums,target,n);
+
+        //res.push_back(FirstOcc);
+        res[0] = FirstOcc;
+        res[1] = LastOcc;
+
+        return res;
+    }
+};
